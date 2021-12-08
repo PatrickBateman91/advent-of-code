fe32a498127a4ee4cb99ca2ae65a92f027af64af
Amar Spahić <amar.spahic@intellegens.hr>
2021-12-08 15:43:34 +0100

    Added solution for Day8 of 2021

--- 2020/Day12/task.js
+++ 2020/Day12/task.js
@@ -84,30 +84,106 @@ const fs = require("fs");
 function getDirectionIndex(direction, sides) {
   for (let i = 0; i < sides.length; i++) {
     if (sides[i] === direction) {
-      return i;
+      return { index: i, waypoint: direction };
     }
   }
 }
 
-function getNewDirection(direction, side, num) {
-  const sides = ["N", "E", "S", "W"];
-  const currentDirectionIndex = getDirectionIndex(direction, sides);
-
-  if (side === "L") {
-    if (currentDirectionIndex - num / 90 < 0) {
-      let diff = sides.length - Math.abs(currentDirectionIndex - num / 90);
-      return sides[diff];
-    } else {
-      return sides[currentDirectionIndex - num / 90];
-    }
+function setPosition(position, waypoint, currentWayPoint, num) {
+  if (currentWayPoint[0] === "N") {
+    position.N += Math.abs(position.S - waypoint[currentWayPoint[0]] * num);
+    position.S = position.S - position.N < 0 ? 0 : position.S - position.N;
   } else {
-    if (currentDirectionIndex + num / 90 >= sides.length) {
-      let diff = Math.abs(sides.length - (currentDirectionIndex + num / 90));
-      return sides[diff];
+    position.S += Math.abs(position.N - waypoint[currentWayPoint[0]] * num);
+    position.N = position.N - position.S < 0 ? 0 : position.N - position.S;
+  }
+
+  if (currentWayPoint[1] === "E") {
+    position.E += Math.abs(position.W - waypoint[currentWayPoint[1]] * num);
+    position.W = position.W - position.E < 0 ? 0 : position.W - position.E;
+  } else if (currentWayPoint[1] === "W") {
+    position.W += Math.abs(position.E - waypoint[currentWayPoint[1]] * num);
+    position.E = position.E - position.W < 0 ? 0 : position.E - position.W;
+  }
+}
+
+function getNewDirection(currentWayPoint, waypoint, side, num) {
+  const sides = ["N", "E", "S", "W"];
+  const oldWaypoint = {
+    E: waypoint.E,
+    W: waypoint.W,
+    N: waypoint.N,
+    S: waypoint.S,
+  };
+
+  const numOfMoves = num / 90;
+  // R
+
+  if (side === "R") {
+    sides.forEach((geographicalSide, index) => {
+      if (index + numOfMoves < sides.length) {
+        waypoint[sides[index + numOfMoves]] = oldWaypoint[sides[index]];
+      } else {
+        let diff = Math.abs(sides.length - (index + numOfMoves));
+        waypoint[sides[diff]] = oldWaypoint[sides[index]];
+      }
+    });
+  } else {
+    sides.forEach((geographicalSide, index) => {
+      if (index - numOfMoves >= 0) {
+        waypoint[sides[index - numOfMoves]] = oldWaypoint[sides[index]];
+      } else {
+        let diff = sides.length - Math.abs(index - numOfMoves);
+        waypoint[sides[diff]] = oldWaypoint[sides[index]];
+      }
+    });
+  }
+
+  const currentDirectionIndexes = [
+    getDirectionIndex(currentWayPoint[0], sides),
+    getDirectionIndex(currentWayPoint[1], sides),
+  ];
+
+  const returnWaypoint = [];
+
+  for (const currentDirection of currentDirectionIndexes) {
+    if (side === "L") {
+      if (currentDirection.index - num / 90 < 0) {
+        let diff = sides.length - Math.abs(currentDirection.index - num / 90);
+
+        if (sides[diff] === "N" || sides[diff] === "S") {
+          returnWaypoint[0] = sides[diff];
+        } else {
+          returnWaypoint[1] = sides[diff];
+        }
+      } else {
+        let newSide = sides[currentDirection.index - num / 90];
+        if (newSide === "N" || newSide === "S") {
+          returnWaypoint[0] = newSide;
+        } else {
+          returnWaypoint[1] = newSide;
+        }
+      }
     } else {
-      return sides[currentDirectionIndex + num / 90];
+      if (currentDirection.index + num / 90 >= sides.length) {
+        let diff = Math.abs(sides.length - (currentDirection.index + num / 90));
+        if (sides[diff] === "N" || sides[diff] === "S") {
+          returnWaypoint[0] = sides[diff];
+        } else {
+          returnWaypoint[1] = sides[diff];
+        }
+      } else {
+        const newSide = sides[currentDirection.index + num / 90];
+        if (newSide === "N" || newSide === "S") {
+          returnWaypoint[0] = newSide;
+        } else {
+          returnWaypoint[1] = newSide;
+        }
+      }
     }
   }
+
+  return returnWaypoint; //?
 }
 
 fs.readFile("input.txt", "utf-8", function (err, data) {
@@ -126,11 +202,9 @@ fs.readFile("input.txt", "utf-8", function (err, data) {
     W: 0,
     N: 1,
     S: 0,
-  }
+  };
 
-  let currentDirection = "E";
-  let currentWayPoint = ["E", "N"]
-
+  let currentWayPoint = ["N", "E"];
 
   arrayOfInputs.forEach((command) => {
     let action = command[0];
@@ -141,24 +215,29 @@ fs.readFile("input.txt", "utf-8", function (err, data) {
       case "S":
       case "E":
       case "W":
-        counter[action] += num;
+        waypoint[action] += num;
         break;
 
       case "L":
       case "R":
-        const newDirection = getNewDirection(currentDirection, action, num);
-        currentDirection = newDirection;
+        currentWayPoint = getNewDirection(
+          currentWayPoint,
+          waypoint,
+          action,
+          num
+        );
         break;
 
       case "F":
-        counter[currentDirection] += num;
+        setPosition(position, waypoint, currentWayPoint, num);
         break;
     }
   });
 
-//   const manhattanDistance =
-//     Math.abs(counter.E - counter.W) + Math.abs(counter.N - counter.S);
-//   console.log(
-//     `Manhattan distance between first location and second is ${manhattanDistance}`
-//   );
+  const manhattanDistance =
+    Math.abs(position.E - position.W) + Math.abs(position.N - position.S);
+
+  console.log(
+    `Manhattan distance between first location and second is ${manhattanDistance}`
+  );
 });
